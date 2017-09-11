aa76c0bf9cf3ab15878a7491a92f363ffd52cffd
Tyson Harper <tysonwharper@gmail.com>
2017-09-10 23:13:32 -0600

    new projects

---
+++ courseContent/js/week_1_projects.js
@@ -0,0 +1,71 @@
+//////////////////PROBLEM 1////////////////////
+
+
+//Create a function called isWeekEnd that accepts a day of the week (Monday - Sunday) as it's only parameter.
+//If the argument you passed in is equal to 'Saturday', return true. If it's not, return false.
+
+//Code Here
+
+//////////////////PROBLEM 2////////////////////
+
+
+//Use the same function in problem 1 and check to see if day is equal to Saturday or Sunday. Return true or false.
+
+
+//Code Here
+
+//////////////////PROBLEM 3////////////////////
+
+
+//Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
+
+
+//Code Here
+
+
+//////////////////PROBLEM 4////////////////////
+
+
+
+//Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
+//then alerts "Welcome, " plus whatever the users name is.
+
+//Code Here
+
+
+
+//////////////////PROBLEM 6////////////////////
+
+
+
+//Create a function called myName that returns your name
+
+//Code Here
+
+
+
+//Now save the function definition of myName into a new variable called newMyName
+
+//Code Here
+
+//Now alert the result of invoking newMyName
+
+
+
+//////////////////PROBLEM 7////////////////////
+
+
+
+//Create a function  to compute the sum of the two given integers. If the two values are same, then returns triple their sum.
+
+//Code Here
+
+
+
+//////////////////PROBLEM 8////////////////////
+
+
+
+//Create a function to check two given numbers and return true if one of the number is 50 or if their sum is 50.
+
+//Code Here
