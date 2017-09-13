1e374a6189fffb2babc4590acf03c3e7e7f50e3a
MPDC109 <MPDC109@STUDENT.SLCC.int>
2017-09-12 20:32:18 -0600

    Add javascript file

---
+++ studentFiles/Bhavani/week1.js
@@ -0,0 +1,111 @@
+//Create a function called isWeekEnd that accepts a day of the week (Monday - Sunday) as it's only parameter.
+//If the argument you passed in is equal to 'Saturday', return true. If it's not, return false.
+
+//Code Here
+var day;
+ function isweekend(day) {
+
+if(day=="Saturday"){
+
+    return true;}
+
+    else
+
+    return false;
+ }
+
+ isweekend("Monday");
+
+
+//////////////////PROBLEM 2////////////////////
+
+
+//Use the same function in problem 1 and check to see if day is equal to Saturday or Sunday. Return true or false.
+
+
+var day;
+function isweekend(day) {
+
+    if(day=="Saturday"||"Sunday")
+     {
+
+        return true;}
+
+    else
+
+        return false;
+}
+
+isweekend("Monday")
+
+//////////////////PROBLEM 3////////////////////
+
+
+//Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
+var x=prompt("name");
+function getname(){
+
+    return x;
+}
+getname();
+
+//////////////////PROBLEM 4////////////////////
+
+
+
+//Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
+//then alerts "Welcome, " plus whatever the users name is.
+var x=prompt("name");
+function getname(){
+
+    return x;
+}
+function welcome(){
+ var  y=getname();
+    alert("welcome," +y);
+}
+
+welcome();
+//////////////////PROBLEM 5////////////////////
+
+
+
+//Create a function called myName that returns your name
+
+//Code Here
+
+
+
+//Now save the function definition of myName into a new variable called newMyName
+
+//Code Here
+
+//Now alert the result of invoking newMyName
+
+
+
+//////////////////PROBLEM 6////////////////////
+
+
+
+//Create a function  to compute the sum of the two given integers. If the two values are same, then returns triple their sum.
+
+funtion triplesum(x,y){
+
+}
+
+
+
+//////////////////PROBLEM 7////////////////////
+
+
+
+//Create a function to check two given numbers and return true if one of the number is 50 or if their sum is 50.
+
+//Code Here
+
+function test50(x,y){
+    return(x==50||y==50||x+y==50);
+}
+
+function test50(10,50);
