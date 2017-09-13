045c32a4b553beedd3990b87c928b3ed0cac8053
Tyson Harper <tysonwharper@gmail.com>
2017-09-12 20:15:47 -0600

    updated files

---
+++ courseContent/fizzBuzz.js
@@ -0,0 +1,17 @@
+for ( var n=100; n >= 1; n--) {
+    var output ="";
+    if (n%3==0){
+        output= "fizz";
+    }
+    if (n%5==0){
+        output = "buzz";
+    }
+    console.log(output || n)
+
+}
+
+var message = "how";
+message = " are";
+message += " you";
+
+console.log(message);
