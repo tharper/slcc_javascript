d3d8dd95fe5c5441c446a2fc4720f4dcdbf0c248
sbhavanipriya <31907166+sbhavanipriya@users.noreply.github.com>
2017-09-27 03:51:23 +0530

    Update week2.js

--- studentFiles/Bhavani/week2.js
+++ studentFiles/Bhavani/week2.js
@@ -71,6 +71,120 @@ console.log(car.checksunroof());
 /*****Atributes:Type
  Methods:Add,Subtract,Multiply,Divide*/
 
-var Calculator={
+var Calculator= {
+    Number: [ 1, 2, 3, 4, 5, 6, 7, 8, 9,0],
+    type: ['Scientific', 'Normal'],
+
+    Addition: function (x, y) {
+
+        return (this.Number[2]+this.Number[3]);
+
+    },
+
+
+    Subraction:function(){
+    return (this.Number[9]-this.Number[6]);
+    },
+
+
+    Multiply:function(){
+
+        return(this.Number[2]*this.Number[5]*this.Number[0]);
+    },
+
+
+    Division:function(){
+
+        return(this.Number[5]%this.Number[1]);
+
+
+        return(this.Number[5]/this.Number[1]);
+
+
+    }
+
+
+    };
+
+
+
+console.log(Calculator.Addition());
+console.log(Calculator.Subraction());
+console.log(Calculator.Multiply());
+console.log(Calculator.Division());
+console.log(Calculator.Division());
+
+
+
+/*Dog
+&&&&&&
+Attribute:Breed,Colour,Eyes,Gender,Weight.
+&&&&&&&&&
+Method:
+@@@@@@@ Bark,Fetch,Grow.
+ */
+
+
+var Dog={
+    Name:"amigo",
+    Breed:"Labrador Retriever",
+    Color:"Yellow",
+    Type:"American lab",
+    Weight:60,
+    Gender:"Male",
+    Ball:0,
+    Danger:0,
+    Treats:0,
+
+    Bark:function(){
+        if(this.Danger == 1){
+            alert("Barks");
+        }
+        else if(this.Danger == 0){
+            alert("Happy Dog");
+        }
+    },
+
+    Greet:function(){
+        alert("Shake Hands");
+    },
+
+    Fetch:function(){
+        if (this.ball == 1){
+            alert(this.Name +" fetched the ball");
+        }
+        else{
+            alert(this.Name +"sad");
+        }
+    },
+
+
+    Grow:function(){
+        if (this.treat == 1){
+            this.Weight=75;
+            return this.Weight;
+        }else  return this.Weight;
+
+}
+
+};
+
+console.log(Dog.Name);
+console.log(Dog.Type);
+console.log(Dog.Breed);
+console.log(Dog.Bark());
+console.log(Dog.Fetch());
+console.log(Dog.Greet());
+console.log(Dog.Grow());
+
+
+/*Radio*/
+/******/
+
+
+var Radio{
+
+
+    brand:'Sony'
 
 }
